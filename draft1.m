ed0543a1bffa001159d6d0ba40252017343cc2e4
maxislash <descos.maxime@gmail.com>
2016-06-07 17:12:05 -0300

    Modification/Optimization of the QAM mapper

--- draft1.m
+++ draft1.m
@@ -4,150 +4,129 @@ close all;
 
 %---------------- Initial Parameters ----------------
 
-n = str2num (input ("Enter the number of bits in the bitstream\n", "s")); %number of bits in the FIFO
-choice = menu ("Which QAM do you want to use", "4", "16", "64");  %number of symbols in the constellation
-if choice == 1
+choice1 = menu ("Which number of bits in the bitstream do you want?", "12", "64", "102", "1020", "10200"); %number of bits in the FIFO
+if choice1 == 1
+  n = 12;
+elseif choice1 == 2
+  n = 64;
+elseif choice1 == 3
+  n = 102;
+elseif choice1 == 4
+  n = 1020;
+elseif choice1 == 5
+  n = 10200;
+endif
+
+choice2 = menu ("Which QAM do you want to use?", "4", "16", "64");  %number of symbols in the constellation
+if choice2 == 1
   M = 4;
-elseif choice == 2
+elseif choice2 == 2
   M = 16;
-elseif choice == 3
+elseif choice2 == 3
   M = 64;
 endif
 k = log2(M); %number of bits in one symbol
 w = 2*pi*10;  % angular frequency
 
+if mod(n, k) ~= 0
+    error('numberOfBits must be a multiple of log2(M).');
+end
+
 %----------------Creation of the bitstream ---------
 
 dataIn = randi(2,n,1) - 1;  % Generate vector of binary data
 
-stem(dataIn,"filled");
-title('Random Bits');
-xlabel('Bit Index');
-ylabel('Binary Value');
+%stem(dataIn,"filled");
+%title('Random Bits');
+%xlabel('Bit Index');
+%ylabel('Binary Value');
 
 %----------------- QAM Mapper --------------------
 
 numberOfSymbols = length(dataIn)/k; %number of symbols in the FIFO
 
-symbol = 0;
+% Define mapping table applying Gray mapping
 
-for i = 1:n % rewshape dataIn into dataInPack with one case for one symbol composed of k bits 
+if M == 4
+  mappingTable(1) =  1 + 1*j;
+  mappingTable(2) = -1 + 1*j;
+  mappingTable(3) =  1 - 1*j;
+  mappingTable(4) = -1 - 1*j;
 
-  if mod(i,k) == 0
-    dataInPack(i/k) = symbol*10 + dataIn(i);
-    symbol = 0;
-  else
-    symbol = symbol*10 + dataIn(i);
-   endif
-   
-endfor
-
-%dataInPack
-
-for i = 1:numberOfSymbols  % symbols : matrix which will contain the value of I(first column) and Q (second column) for each symbol
-
-  symbolDe  = bin2dec(mat2str(dataInPack(i)));
-  
-  symbols(i,1) = 0;
-  symbols(i,2) = 0;
-  
-  if M == 64
-  
-    if mod((symbolDe+2),8) == 0
-      symbols(i,1) = +1;
-    elseif mod((symbolDe+1),8) == 0
-      symbols(i,1) = +3;
-    elseif mod((symbolDe+3),8) == 0
-      symbols(i,1) = +5;
-    elseif mod((symbolDe+4),8) == 0
-      symbols(i,1) = +7;
-    elseif mod((symbolDe+6),8) == 0
-      symbols(i,1) = -1;
-    elseif mod((symbolDe+5),8) == 0
-      symbols(i,1) = -3;
-    elseif mod((symbolDe+7),8) == 0
-      symbols(i,1) = -5;
-    elseif mod(symbolDe,8) == 0
-      symbols(i,1) = -7;
+elseif M == 16
+  mappingTable(1:4) = -3;
+  mappingTable(5:8) = -1;
+  mappingTable(9:12) = +3;
+  mappingTable(13:16) = +1;
+  for i = 0:15
+    if mod(i,4) == 0
+      mappingTable(i+1) = mappingTable(i+1) -3*j;
+    elseif mod(i+3,4) == 0
+      mappingTable(i+1) = mappingTable(i+1) -1*j;
+    elseif mod(i+1,4) == 0
+      mappingTable(i+1) = mappingTable(i+1) +1*j;
+    elseif mod(i+2,8) == 0
+      mappingTable(i+1) = mappingTable(i+1) +3*j;
     endif
-  
-    if and(16 <= symbolDe, symbolDe < 24)
-      symbols(i,2) = +1;
-    elseif and(24 <= symbolDe, symbolDe < 32)
-      symbols(i,2) = +3;
-    elseif and(8 <= symbolDe, symbolDe < 16)
-      symbols(i,2) = +5;
-    elseif and(0 <= symbolDe, symbolDe < 8)
-      symbols(i,2) = +7;
-    elseif and(48 <= symbolDe, symbolDe < 56)
-      symbols(i,2) = -1;
-    elseif and(56 <= symbolDe, symbolDe < 64)
-      symbols(i,2) = -3;
-    elseif and(40 <= symbolDe, symbolDe < 48)
-      symbols(i,2) = -5;
-    elseif and(32 <= symbolDe, symbolDe < 40)
-      symbols(i,2) = -7;
-    endif
-      
-  elseif M == 16
-  
-    if mod(symbolDe,4) == 0
-      symbols(i,2) = -3;
-    elseif mod((symbolDe + 2),4) == 0
-      symbols(i,2) = +3; 
-    elseif mod((symbolDe + 3),4) == 0
-      symbols(i,2) = -1;
-    elseif mod((symbolDe + 1),4) == 0
-      symbols(i,2) = +1;
-    endif
-      
-    if symbolDe < 4
-      symbols(i,1) = -3;
-    elseif and(4 <= symbolDe, symbolDe < 8)
-      symbols(i,1) = -1;
-    elseif and(8 <= symbolDe, symbolDe < 12)
-      symbols(i,1) = +3;
-    elseif and(12 <= symbolDe, symbolDe < 16)
-      symbols(i,1) = +1;
-    endif
+  endfor
     
-  elseif M == 4
-    if mod(symbolDe,2) == 0
-      symbols(i,1) = +1;
-    elseif mod((symbolDe + 1),2) == 0
-      symbols(i,1) = -1;
+  
+elseif M == 64
+  mappingTable(1:8)   = + 7*j;
+  mappingTable(9:16)  = + 5*j;
+  mappingTable(17:24) = + 1*j;
+  mappingTable(25:32) = + 3*j;
+  mappingTable(33:40) = - 7*j;
+  mappingTable(41:48) = - 5*j;
+  mappingTable(49:56) = - 1*j;
+  mappingTable(57:64) = - 3*j;
+  
+  for i = 0:63
+    if mod(i+2,8) == 0
+      mappingTable(i+1) = mappingTable(i+1) +1;
+    elseif mod(i+1,8) == 0
+      mappingTable(i+1) = mappingTable(i+1) +3;
+    elseif mod(i+3,8) == 0
+      mappingTable(i+1) = mappingTable(i+1) +5;
+    elseif mod(i+4,8) == 0
+      mappingTable(i+1) = mappingTable(i+1) +7;
+    elseif mod(i+6,8) == 0
+      mappingTable(i+1) = mappingTable(i+1) -1;
+    elseif mod(i+5,8) == 0
+      mappingTable(i+1) = mappingTable(i+1) -3;
+    elseif mod(i+7,8) == 0
+      mappingTable(i+1) = mappingTable(i+1) -5;
+    elseif mod(i,8) == 0
+      mappingTable(i+1) = mappingTable(i+1) -7;
     endif
-      
-    if symbolDe < 2
-      symbols(i,2) = +1;
-    elseif 1 < symbolDe < 4
-      symbols(i,2) = -1;
-    endif   
-  endif
+  endfor
+endif
 
+mappedSymbols = zeros(1, numberOfSymbols);
+
+% Map bits to symbols
+for i = 1:k:length(dataIn)
+   
+    symbolBits = dataIn(i:i+k-1);
+    
+    if M == 4
+      symbolIndex = 2^1 * symbolBits(1) + 2^0 * symbolBits(2);
+    elseif M == 16
+      symbolIndex = 2^3 * symbolBits(1) + 2^2 * symbolBits(2) + 2^1 * symbolBits(3) + 2^0 * symbolBits(4);
+    elseif M == 64
+      symbolIndex = 2^5 * symbolBits(1) + 2^4 * symbolBits(2) + 2^3 * symbolBits(3) + 2^2 * symbolBits(4) +  2^1 * symbolBits(5) + 2^0 * symbolBits(6);
+    endif
+
+     % Mapping
+    mappedSymbols((i - 1)/k + 1) = mappingTable( symbolIndex + 1);
 endfor
-
-%symbols
 
 %------------------------------Construction and plot of the signal-----------
 
-signal = symbols(:,1) + j*symbols(:,2);
+signal = mappedSymbols;
 
-scatterplot(signal);
-title('Constellation before sending');
-grid;
-xlabel('I');
-ylabel('Q');
-if M == 4
-  axis([-2 2 -2 2]);
-elseif M == 16
-  axis([-4 4 -4 4]);
-elseif M == 64
-  axis([-8 8 -8 8]);
-endif
-
-t = 0:1/k:length(dataIn)-1/k;
-modulation = real(signal)*cos(w*t) + imag(signal)*sin(w*t);
+t = 0:1/k:length(dataIn)/(k*k)-1/k;
+modulation = real(signal).*cos(w.*t) + imag(signal).*sin(w.*t);
 
 %--------------------------------Channel with noise addition----------------
 
@@ -159,8 +138,9 @@ signalAfter = awgn(signal,snr,'measured');
 
 %----------------------------- Deconstruction of the signal and plot of the received signal--------------
 
-scatterplot(signalAfter);
-title('Constellation after sending');
+sPlotFig = scatterplot(signal,1,0,'k*');
+hold on
+scatterplot(signalAfter,1,0,'g.', sPlotFig);
 grid;
 xlabel('I');
 ylabel('Q');
@@ -171,10 +151,33 @@ elseif M == 16
 elseif M == 64
   axis([-8 8 -8 8]);
 endif
+title('Signal before and after sending');
+legend('Before Sending','After Sending');
 
 phi = 0;
 
-signalAfterDeconstructionI = signalAfter*cos(w*t + phi);
-signalAfterDeconstructionQ = signalAfter*sin(w*t + phi);
+signalAfterDeconstructionI = signalAfter.*cos(w*t + phi);
+signalAfterDeconstructionQ = signalAfter.*sin(w*t + phi);
 
 %-------------------------------FIR-------------------------------------------
+
+% Low pass filtering with a Butterworth filter
+[b,a]=butter(2,0.4);
+Yi=filter(b,a,signalAfterDeconstructionI);
+Yk=filter(b,a,signalAfterDeconstructionQ);
+
+%----------------------------- QAM demapper -------------------------------
+
+receivedSignal = Yi + j*Yk;
+for i = 1:length(receivedSignal)
+  [mindiff minIndex] = min(receivedSignal(i) - mappingTable);
+  symbolIndex = minIndex - 1;
+  bitString = dec2bin(symbolIndex, 4);
+end
+
+  receivedBits((i-1)*4 + 1) = str2double(bitString(1));
+  receivedBits((i-1)*4 + 2) = str2double(bitString(2));
+  receivedBits((i-1)*4 + 3) = str2double(bitString(3));
+  receivedBits((i-1)*4 + 4) = str2double(bitString(4));
+receivedBits = zeros(1, numberOfBits / 4);
+
